c6ee1405b9c1c298536d787a96ad4e92afc79f93
Easy <easyxy123@gmail.com>
2021-07-20 14:57:31 +0200

    Add files via upload

--- java/com/easy/passbase/MainActivity.java
+++ java/com/easy/passbase/MainActivity.java
@@ -3,13 +3,20 @@ package com.easy.passbase;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import static com.easy.passbase.PasswordDB.getPassword;
 import static com.easy.passbase.PasswordDB.passwordDB;
-import static com.easy.passbase.PasswordDBHelper.*;
+import static com.easy.passbase.PasswordDBHelper.AMOUNT_OF_SELECTABLE_COLUMNS;
+import static com.easy.passbase.PasswordDBHelper.COLUMN_EMAIL;
+import static com.easy.passbase.PasswordDBHelper.COLUMN_NAME;
+import static com.easy.passbase.PasswordDBHelper.COLUMN_NOTES;
+import static com.easy.passbase.PasswordDBHelper.COLUMN_PASSWORD;
+import static com.easy.passbase.PasswordDBHelper.COLUMN_USERNAME;
+import static com.easy.passbase.PasswordDBHelper.selectableColumns;
 
 public class MainActivity extends AppCompatActivity {
     private Cursor currentCursor;
@@ -18,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtSelectedEmail;
     private TextView txtSelectedUsername;
     private TextView txtSelectedNotes;
+    private ImageButton ibtCopyPassword;
+    private ImageButton ibtCopyEmail;
+    private ImageButton ibtCopyUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
         txtSelectedEmail = findViewById(R.id.txt_selectedEmail);
         txtSelectedUsername = findViewById(R.id.txt_selectedUsername);
         txtSelectedNotes = findViewById(R.id.txt_selectedNotes);
+
+        ibtCopyPassword = findViewById(R.id.ibt_copyPassword);
+        ibtCopyEmail = findViewById(R.id.ibt_copyEmail);
+        ibtCopyUsername = findViewById(R.id.ibt_copyUsername);
+
+        checkVisibility();
     }
 
     public void openSelectEntry(View v) {
@@ -49,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
             entry[i] = getEntry(cursor, selectableColumns[i]);
             setTextViewCheckNull(selectableColumns[i], entry[i]);
         }
+        checkVisibility();
     }
 
     private String getEntry(Cursor cursor, String column) {
@@ -69,6 +86,31 @@ public class MainActivity extends AppCompatActivity {
                 return txtSelectedNotes;
             default:
                 return null;
+        }
+    }
+
+    private ImageButton getImageButtonByColumn(String column) {
+        switch (column) {
+            case COLUMN_PASSWORD:
+                return ibtCopyPassword;
+            case COLUMN_EMAIL:
+                return ibtCopyEmail;
+            case COLUMN_USERNAME:
+                return ibtCopyUsername;
+            default:
+                return null;
+        }
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private void checkVisibility() {
+        //Index 1 to 3 meaning only Password, Email and Username are being checked
+        //as these are the only ones with copy buttons
+        for (int i = 1; i < AMOUNT_OF_SELECTABLE_COLUMNS - 1; ++i) {
+            if (getTextViewByColumn(selectableColumns[i]).getText().equals(getString(R.string.unavailable_info)))
+                getImageButtonByColumn(selectableColumns[i]).setVisibility(View.INVISIBLE);
+            else
+                getImageButtonByColumn(selectableColumns[i]).setVisibility(View.VISIBLE);
         }
     }
 
